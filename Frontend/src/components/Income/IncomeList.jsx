571d7407128056e409293dcce66df75e61c9a6dc
anshu2298 <amshusingh2298@gmail.com>
2025-04-10 11:19:12 +0530

    minor changes

--- Frontend/src/components/Income/IncomeList.jsx
+++ Frontend/src/components/Income/IncomeList.jsx
@@ -1,20 +1,23 @@
 import React from "react";
-import TransactionInfoCard from "../cards/TransactionInfoCard";
+import TransactionInfoCard from "../Cards/TransactionInfoCard";
 import moment from "moment";
 import { LuDownload } from "react-icons/lu";
 
 const IncomeList = ({ transactions, onDelete, onDownload }) => {
   return (
-    <div className="card">
-      <div className="flex items-center justify-between">
-        <h5 className="text-lg">Income Sources</h5>
+    <div className='card'>
+      <div className='flex items-center justify-between'>
+        <h5 className='text-lg'>Income Sources</h5>
 
-        <button className="card-btn" onClick={onDownload}>
-          <LuDownload className="text-base" /> Download
+        <button
+          className='card-btn'
+          onClick={onDownload}
+        >
+          <LuDownload className='text-base' /> Download
         </button>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2">
+      <div className='grid grid-cols-1 md:grid-cols-2'>
         {transactions?.map((income) => (
           <TransactionInfoCard
             key={income._id}
@@ -22,7 +25,7 @@ const IncomeList = ({ transactions, onDelete, onDownload }) => {
             icon={income.icon}
             date={moment(income.date).format("Do MMM YYYY")}
             amount={income.amount}
-            type="income"
+            type='income'
             onDelete={() => onDelete(income._id)}
           />
         ))}
